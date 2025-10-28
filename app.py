99d3a7cb0b66d31b56cff29ce23c54e79a0ef6f0
BOPOH <nikolaigarshin2112@gmail.com>
2025-10-28 15:54:23 +0300

    Grammar corrected.

--- app.py
+++ app.py
@@ -7,18 +7,18 @@
 #                              ╚═════╝░░╚════╝░╚═╝░░░░░░╚════╝░╚═╝░░╚═╝
 from flask import Flask
 
-from functions import get_movie_or_TV_show_by_title, get_movie_or_TV_show_by_year_range
+from functions import get_movie_or_TV_series_by_title, get_movie_or_TV_series_by_year_range
 
 app = Flask(__name__)
 
 @app.route('/movie/<title>')
-def movie_or_TV_show_page(title):
-    return f"{get_movie_or_TV_show_by_title(title)}"
+def movie_or_TV_series_page(title):
+    return f"{get_movie_or_TV_series_by_title(title)}"
 
 
 @app.route('/movie/<start_year>/to/<end_year>')
-def movie_or_TV_show_page_by_years(start_year, end_year):
-    return f"{get_movie_or_TV_show_by_year_range(start_year, end_year)}"
+def movie_or_TV_series_page_by_years(start_year, end_year):
+    return f"{get_movie_or_TV_series_by_year_range(start_year, end_year)}"
 
 
 if __name__ == '__main__':
--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
 import sqlite3
 
 
-def get_movie_or_TV_show_by_title(title):
+def get_movie_or_TV_series_by_title(title):
     """
-    The function searches for a movie or TV show in the database by its title.
+    The function searches for a movie or TV series in the database by its title.
     :param title: Users query.
     :return: Dictionary with category and search result.
     """
@@ -17,17 +17,16 @@ def get_movie_or_TV_show_by_title(title):
             LIMIT 1
             """
         cursor.execute(query)
-        movie_search_result = cursor.fetchall()[0]
+        movie_or_tv_series_search_result = cursor.fetchall()[0]
         keys = ['title', 'country', 'release_year', 'listed_in', 'description']
-        result = dict(zip(keys, movie_search_result))
+        result = dict(zip(keys, movie_or_tv_series_search_result))
         return result
 
 
-def get_movie_or_TV_show_by_year_range(start_year, end_year):
-    """
-
-    :param start_year:
-    :param end_year:
+def get_movie_or_TV_series_by_year_range(start_year, end_year):
+    """A function that searches for a movie or TV series within a specified time period by year of release.
+    :param start_year:The year of manufacture from which to search.
+    :param end_year: Year of manufacture up to which to search.
     :return:
     """
     with sqlite3.connect('netflix.db') as connection:
@@ -39,11 +38,11 @@ def get_movie_or_TV_show_by_year_range(start_year, end_year):
             LIMIT 100
             """
         cursor.execute(query)
-        movies_or_tv_show_search_result = cursor.fetchall()
-        movies_or_tv_show_list = []
+        movies_or_tv_shows_search_result = cursor.fetchall()
+        movies_or_tv_shows_list = []
 
-        for movies_or_TV_show in movies_or_tv_show_search_result:
+        for movies_or_TV_show in movies_or_tv_shows_search_result:
             keys = ['title', 'release_year']
-            movies_or_tv_show_list.append(dict(zip(keys, movies_or_TV_show)))
+            movies_or_tv_shows_list.append(dict(zip(keys, movies_or_TV_show)))
 
-        return movies_or_tv_show_list
+        return movies_or_tv_shows_list
