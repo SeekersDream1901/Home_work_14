5689a38cf02a2d1b9a9bfb3ae2afd716c16418e0
BOPOH <nikolaigarshin2112@gmail.com>
2025-10-28 14:55:52 +0300

    Added a viewport /movie/<title> for implement the function get_movie_or_TV_show_by_title(title).

--- app.py
+++ app.py
@@ -5,3 +5,16 @@
 #                              ██╔══██╗██║░░██║██╔═══╝░██║░░██║██╔══██║
 #                              ██████╦╝╚█████╔╝██║░░░░░╚█████╔╝██║░░██║
 #                              ╚═════╝░░╚════╝░╚═╝░░░░░░╚════╝░╚═╝░░╚═╝
+from flask import Flask
+
+from functions import get_movie_or_TV_show_by_title
+
+app = Flask(__name__)
+
+@app.route('/movie/<title>')
+def movie_or_TV_show_page(title):
+    return f"{get_movie_or_TV_show_by_title(title)}"
+
+
+if __name__ == '__main__':
+    app.run()
