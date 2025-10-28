47c6d1a3aac0b37d3bc85ee19fbe8634303df145
BOPOH <nikolaigarshin2112@gmail.com>
2025-10-28 15:43:59 +0300

    Added a viewport /movie/<start_year>/to/<end_year> for implement the function movie_or_TV_show_page_by_years(start_year, end_year).

--- app.py
+++ app.py
@@ -7,13 +7,18 @@
 #                              ╚═════╝░░╚════╝░╚═╝░░░░░░╚════╝░╚═╝░░╚═╝
 from flask import Flask
 
-from functions import get_movie_or_TV_show_by_title
+from functions import get_movie_or_TV_show_by_title, get_movie_or_TV_show_by_year_range
 
 app = Flask(__name__)
 
 @app.route('/movie/<title>')
 def movie_or_TV_show_page(title):
     return f"{get_movie_or_TV_show_by_title(title)}"
+
+
+@app.route('/movie/<start_year>/to/<end_year>')
+def movie_or_TV_show_page_by_years(start_year, end_year):
+    return f"{get_movie_or_TV_show_by_year_range(start_year, end_year)}"
 
 
 if __name__ == '__main__':
