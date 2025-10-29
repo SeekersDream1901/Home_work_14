1ae745ac31406dcb920c2d3fcf261aa600cd1c4a
BOPOH <nikolaigarshin2112@gmail.com>
2025-10-29 21:31:31 +0300

    As part of step 6 of the homework assignment, the following was completed: 1) A function was implemented that will allow the user to pass in the type of film (movie or series), release year, and genre, and return a list of film titles with their descriptions in JSON.
    To speed up the program and implement OOP (encapsulation), some of the code was moved to a separate function, specifically connecting to the database and executing the query.

--- functions.py
+++ functions.py
@@ -3,26 +3,30 @@ import sqlite3
 import json
 
 
+def connect_to_database_and_query_execution(query):
+    with sqlite3.connect('netflix.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+
+
 def get_movie_or_TV_series_by_title(title):
     """
     The function searches for a movie or TV series in the database by its title.
     :param title: Users query.
     :return: Dictionary with category and search result.
     """
-    with sqlite3.connect('netflix.db') as connection:
-        cursor = connection.cursor()
-        query = f"""
-            SELECT title, country, release_year, listed_in, description
-            FROM netflix
-            WHERE title LIKE '%{title}%'
-            ORDER BY release_year DESC
-            LIMIT 1
-            """
-        cursor.execute(query)
-        movie_or_tv_series_search_result = cursor.fetchall()[0]
-        keys = ['title', 'country', 'release_year', 'listed_in', 'description']
-        result = dict(zip(keys, movie_or_tv_series_search_result))
-        return result
+    query = f"""
+        SELECT title, country, release_year, listed_in, description
+        FROM netflix
+        WHERE title LIKE '%{title}%'
+        ORDER BY release_year DESC
+        LIMIT 1
+        """
+    movie_or_tv_series_search_result = connect_to_database_and_query_execution(query)[0]
+    keys = ['title', 'country', 'release_year', 'listed_in', 'description']
+    result = dict(zip(keys, movie_or_tv_series_search_result))
+    return result
 
 
 def get_movie_or_TV_series_by_year_range(start_year, end_year):
@@ -31,23 +35,18 @@ def get_movie_or_TV_series_by_year_range(start_year, end_year):
     :param end_year: Year of manufacture up to which to search.
     :return: List of dictionaries of films and/or TV series released in the specified years of release.
     """
-    with sqlite3.connect('netflix.db') as connection:
-        cursor = connection.cursor()
-        query = f"""
-            SELECT title, release_year
-            FROM netflix
-            WHERE {start_year} <= release_year <= {end_year}
-            LIMIT 100
-            """
-        cursor.execute(query)
-        movies_or_tv_shows_search_result = cursor.fetchall()
-        movies_or_tv_shows_list = []
-
-        for movies_or_TV_show in movies_or_tv_shows_search_result:
-            keys = ['title', 'release_year']
-            movies_or_tv_shows_list.append(dict(zip(keys, movies_or_TV_show)))
-
-        return movies_or_tv_shows_list
+    query = f"""
+        SELECT title, release_year
+        FROM netflix
+        WHERE {start_year} <= release_year <= {end_year}
+        LIMIT 100
+        """
+    movies_or_tv_shows_search_result = connect_to_database_and_query_execution(query)
+    movies_or_tv_shows_list = []
+    for movies_or_TV_show in movies_or_tv_shows_search_result:
+        keys = ['title', 'release_year']
+        movies_or_tv_shows_list.append(dict(zip(keys, movies_or_TV_show)))
+    return movies_or_tv_shows_list
 
 
 def get_filter_movies_or_tv_series_by_rating(rating):
@@ -56,21 +55,17 @@ def get_filter_movies_or_tv_series_by_rating(rating):
     :param rating: Requested rating.
     :return: List of dictionaries of films and/or TV series by the desired rating.
     """
-    with sqlite3.connect('netflix.db') as connection:
-        cursor = connection.cursor()
-        query = f"""
-            SELECT rating
-            FROM netflix
-            WHERE rating LIKE '{rating}'
+    query = f"""
+        SELECT rating
+        FROM netflix
+        WHERE rating LIKE '{rating}'
         """
-        cursor.execute(query)
-        movies_or_tv_shows_search_result = cursor.fetchall()
-        movies_or_tv_shows_list = []
-        for movies_or_TV_show in movies_or_tv_shows_search_result:
-            keys = ['title', 'rating', 'description']
-            movies_or_tv_shows_list.append(dict(zip(keys, movies_or_TV_show)))
-
-        return movies_or_tv_shows_list
+    movies_or_tv_shows_search_result = connect_to_database_and_query_execution(query)
+    movies_or_tv_shows_list = []
+    for movies_or_TV_show in movies_or_tv_shows_search_result:
+        keys = ['title', 'rating', 'description']
+        movies_or_tv_shows_list.append(dict(zip(keys, movies_or_TV_show)))
+    return movies_or_tv_shows_list
 
 
 def get_10_movies_or_TV_series_released_recently_by_listed_in(listed_in):
@@ -79,24 +74,19 @@ def get_10_movies_or_TV_series_released_recently_by_listed_in(listed_in):
     :param listed_in: The desired genre.
     :return: The 10 most recently released movies and/or TV series of a certain genre in JSON format.
     """
-    with sqlite3.connect('netflix.db') as connection:
-        cursor = connection.cursor()
-        query = f"""
+    query = f"""
         SELECT title, description
         FROM netflix
         WHERE listed_in LIKE '%{listed_in}%'
         ORDER BY release_year DESC
         LIMIT 10
         """
-        cursor.execute(query)
-        movies_or_tv_shows_search_result = cursor.fetchall()
-        movies_or_tv_shows_list = []
-        for movies_or_TV_show in movies_or_tv_shows_search_result:
-            keys = ['title', 'description']
-            movies_or_tv_shows_list.append(dict(zip(keys, movies_or_TV_show)))
-
-        movies_or_tv_shows_json = json.dumps(movies_or_tv_shows_list, ensure_ascii=False, indent=4)
-
+    movies_or_tv_shows_search_result = connect_to_database_and_query_execution(query)
+    movies_or_tv_shows_list = []
+    for movies_or_TV_show in movies_or_tv_shows_search_result:
+        keys = ['title', 'description']
+        movies_or_tv_shows_list.append(dict(zip(keys, movies_or_TV_show)))
+    movies_or_tv_shows_json = json.dumps(movies_or_tv_shows_list, ensure_ascii=False, indent=4)
     return movies_or_tv_shows_json
 
 
@@ -106,12 +96,27 @@ def get_names_of_actors_playing_in_more_two_films_with_the_actors_looking_for(na
     A function that takes the names of two actors as an argument, stores all actors from the cast column, and returns
     a list of those who have played with them in pairs more than 2 times.
     """
-    with sqlite3.connect('netflix.db') as connection:
-        cursor = connection.cursor()
-        cursor.execute(
-            f"""SELECT COUNT(`cast`), `cast`
-            FROM netflix
-            WHERE `cast` LIKE '%{name_of_the_first_actor}%' AND `cast` LIKE '%{name_of_the_second_actor}%'
-            ORDER BY `cast`
-            """)
-        return cursor.fetchall()
+    query = f"""SELECT COUNT(`cast`), `cast`
+        FROM netflix
+        WHERE `cast` LIKE '%{name_of_the_first_actor}%' AND `cast` LIKE '%{name_of_the_second_actor}%'
+        ORDER BY `cast`
+        """
+    return connect_to_database_and_query_execution(query)
+
+
+def find_movie(type, release_year, listed_in):
+    """
+    A function that can be used to pass in the type of film (movie or series), the year of release, and its genre, and
+    return a list of film titles with their descriptions in JSON.
+    """
+    query = f"""SELECT title, description
+        FROM netflix
+        WHERE type LIKE '%{type}%' AND release_year = {release_year} AND listed_in LIKE '%{listed_in}%'
+        LIMIT 10
+        """
+    data = connect_to_database_and_query_execution(query)
+    film_list = []
+    for item in data:
+        film = {"title": item[0], "description": item[1]}
+        film_list.append(film)
+    return film_list
